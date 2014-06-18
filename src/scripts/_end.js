159dc6c971af2268b61e1fb0a07783bb7c58c38a
Kevin Chan <chessmasterhong@yahoo.com>
2014-06-17 21:36:59 -0400

    Added start and end script wrapper files.

---
+++ src/scripts/_end.js
@@ -0,0 +1,4 @@
+    // Ask almond to synchronously require the module value for 'game' here and
+    // return it as the value to use for the public API for the built file.
+    return require('game/game');
+}));
---
+++ src/scripts/_start.js
@@ -0,0 +1,12 @@
+(function(window, factory) {
+    if(typeof define === 'function' && define.amd) {
+        // AMD
+        define(factory);
+    } else if(typeof exports === 'object') {
+        // CommonJS
+        module.exports = factory();
+    } else {
+        // Browser global
+        window.Game = factory();
+    }
+} (this, function() {
