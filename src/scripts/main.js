1fc5f9d6adcbd74b7d00e05d6a62e38da7c11bbd
Kevin Chan <chessmasterhong@yahoo.com>
2014-06-28 12:54:39 -0400

    Minor refactoring for main requires list.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,19 +4,23 @@ requirejs.config({
     baseUrl: './scripts',
     paths: {
         almond: '../scripts/bower_components/almond/almond',
-        phaser: '../scripts/bower_components/phaser/index',
+        Phaser: '../scripts/bower_components/phaser/index',
 
         game: 'game/states/game',
         preloader: 'game/states/preloader'
     },
-    shim: {
-        phaser: { exports: 'Phaser' }
-    }
 });
 
 require([
-    'phaser', 'game', 'preloader'
-], function(Phaser, game, preloader) {
+    'require',
+    'Phaser',
+    'game',
+    'preloader'
+], function(require) {
+    var Phaser = require('Phaser'),
+        game = require('game'),
+        preloader = require('preloader');
+
     var phaserGame = new Phaser.Game(800, 600, Phaser.AUTO, 'gameContainer');
 
     phaserGame.state.add('Game', game);
